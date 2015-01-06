989081594b405369bfd4c689736895a26fbedda5
Chris Hinsley <chris.hinsley@gmail.com>
2015-01-06 21:31:01 +0000

    Track go version

--- chess-2.7.py
+++ chess-2.7.py
@@ -211,7 +211,7 @@ def best_move(board, colour):
 	best_board, best_ply_board, start_time = board, board, time.time()
 	for ply in xrange(1, MAX_PLY):
 		print '\nPly =', ply
-		alpha, beta = -KING_VALUE * 10, KING_VALUE * 10
+		alpha, beta = -KING_VALUE * 100, KING_VALUE * 100
 		for new_board in all_boards:
 			score = -next_move(new_board[1], -colour, -beta, -alpha, ply - 1)
 			if (time.time() - start_time) > MAX_TIME_PER_MOVE:
